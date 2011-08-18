c8fd998452cbeac4785962d95fa147ab5db1fd0b
Hylon Perchik <hypothermia.frost@gmail.com>
2011-08-18 10:35:16 +0100

    added timers, changed tile rendering

--- main.lua
+++ main.lua
@@ -1,25 +1,58 @@
 --tileSize
 tileSize=48
 
+--timers
+timer = { n=0,maxn=5,times={},cb={} }
+
+function timer.new(ms,cb)
+	if timer.n>=timer.maxn then print("out of timers")
+	else
+		timer.times[timer.n]=ms
+		timer.cb[timer.n]=cb
+		timer.n=timer.n+1
+	end
+end
+
+function timer.update(ms)
+	--decrease counters
+	for i=0,timer.n-1 do
+		timer.times[i]=timer.times[i]-ms
+		if timer.times[i]<=0 then
+			timer.cb[i]()
+			--bring down the other counters
+			for j=i+1,timer.n-1 do
+				timer.times[j-1]=timer.times[j]
+				timer.cb[j-1]=timer.cb[j]
+			end
+			timer.n=timer.n-1
+		end
+	end
+end
+
 --how many frames we still need to draw
 redrawCount=1
 
-function redraw()
-	if redrawCount==0 then redrawCount=1 end
+function redraw(frames)
+	if frames==nil then frames=1 end
+	if redrawCount<frames then redrawCount=frames end
+end
+
+-- not really safe but rather argument fixing
+function image:safeDraw(x,y,sx,sy,sw,sh)
+	self:draw(x-sx,y-sy,x,y,sw,sh)
 end
 
 --ui + hud
 dialogTextColor,dialogBackColor,dialogOutColor=color.new(255,255,255),color.new(32,32,32,200),color.new(0,0,0)
 healthbarColor=color.new(232,0,0)
 
-dialogVisible,dialogWho,dialogWhat,dialogTime,dialogDuration=false,nil,nil,0,0
+dialogVisible,dialogWho,dialogWhat=false,nil,nil
 
 function popup(who,msg)
 	dialogVisible=true
 	dialogWho=who
 	dialogWhat=msg
-	dialogTime=0
-	dialogDuration=1000
+	timer.new(1000,function() dialogVisible=false;redraw() end)
 	redraw()
 end
 
@@ -33,9 +66,61 @@ function dialogDraw()
 	screen.drawrect(8,ScreenHeight-40,40,ScreenHeight-8,dialogOutColor)
 end
 
---current world(level/room)
---x,y : view offset
-world={ x=0,y=0,background=image.load("gfx/grassback.png"),prev=nil }
+function drawBar(x,y,w,h,v,c)
+	screen.fillrect(x,y,w,h,dialogBackColor)
+	screen.fillrect(x,y,v*w,h,c)
+	screen.drawrect(x,y,x+w-1,y+h-1,dialogOutColor)
+end
+
+function showLoadingScreen()
+	screen.fillrect(0,0,0,0,dialogOutColor)
+	text.size(22)
+	text.color(dialogTextColor)
+	text.draw(0,ScreenHeight/2-11,"Loading...","center",ScreenWidth)
+	screen.update()
+end
+
+--world is a level/area in game
+Environment = {}
+Environment.__index=Environment
+
+--sets current world
+function Environment.currentWorld(w)
+	--clear up prev world
+	if world~=nil then
+		world:clear()
+		world=nil
+		--collectgarbage()
+	end
+	world=w
+	--load new world
+	world:load()
+end
+
+function Environment:load()
+	self.backgroundImg=image.load(self.background) --??
+end
+
+function Environment:clear()
+	
+end
+
+function Environment:draw()
+	self[0]:draw(viewx,viewy,self.backgroundImg) --1st node
+end
+
+function Environment:canMove(x,y,dir,x2,y2)
+	return self[0]:canMove(x,y,dir,x2,y2)
+end
+
+
+
+--current world
+world = setmetatable({ background="grass.png",w=1,h=1 },Environment)
+Environment.currentWorld(world)
+
+--current world view offset
+viewx,viewy=0,0
 
 --dbg
 globalTime,renderTime=0,0
@@ -46,57 +131,58 @@ function createCharacter(cx,cy,image)
 end
 
 function drawCharacter(char)
-	char.img:safeDraw(char.x-world.x,char.y-world.y-4,char.frame*tileSize,char.dir*tileSize,tileSize,tileSize)
+	char.img:safeDraw(char.x-viewx,char.y-viewy-4,char.frame*tileSize,char.dir*tileSize,tileSize,tileSize)
 end
 
-hero = createCharacter(tileSize*4,tileSize*2,image.load("guy2.png"))
+hero = createCharacter(tileSize*4,tileSize*2,image.load("gfx/monsters/s.png"))
 enemy= createCharacter(240,96,image.load("gfx/npc2.png"))
 
 popup(hero,"hi")
 
 function moveCharacter(char,x,y)
-	if not node:isFree(char.x+x,char.y+y) then popup(char,"Can't go there!");return end
+	if not world:canMove(char.x,char.y,char.dir,char.x+x,char.y+y) then hero.hp=hero.hp-5;popup(char,"Can't go there!");return end
 	--print("move character "..char.x..","..char.y)
-	redrawCount=3
+	redraw(3)
 	local dx,dy=x/3,y/3
-	char.frame=2
+	char.frame=0
 	char.x=char.x+dx
 	char.y=char.y+dy
-	world.x=world.x+dx
-	world.y=world.y+dy
+	viewx=viewx+dx
+	viewy=viewy+dy
 	update()
 	char.frame=0
 	char.x=char.x+dx
 	char.y=char.y+dy
-	world.x=world.x+dx
-	world.y=world.y+dy
+	viewx=viewx+dx
+	viewy=viewy+dy
 	update()
 	char.frame=1
 	char.x=char.x+dx
 	char.y=char.y+dy
-	world.x=world.x+dx
-	world.y=world.y+dy
+	viewx=viewx+dx
+	viewy=viewy+dy
 	update()
 	--print(" done"..char.x..","..char.y)
 end
 
 
-function image:safeDraw(x,y,sx,sy,sw,sh)
-	self:draw(x-sx,y-sy,x,y,sw,sh)
-end
 
-
-
+-- node is a 32x32 array of tiles stored as 2 bytes each representing an index of tile from globalt tile table
 Node = { }
 Node.__index=Node
 
 function Node.new()
-	return setmetatable({ x=0,y=0,x2=32,y2=32,size=32 },Node)
+	return setmetatable({ x=0,y=0,size=32,msize=64 },Node)
 end
 
 function Node:fill()
 	self.data=string.rep('\000',self.size*self.size*2)
 	print("filling node ",self.data:len())
+end
+
+function Node:load(file)
+	self.data=io.read(self.size*self.size*2)
+	print("node loaded!")
 end
 
 function Node:set(obj,x,y)
@@ -111,33 +197,47 @@ function Node:dump()
 	end
 end
 
-function Node:isFree(x,y)
+function Node:canMove(x,y,dir,x2,y2)
+	--can we move from current tile in a given direction?
 	local index=math.floor(y/tileSize)*self.size*2+math.floor(x/tileSize)*2
+	local l,h=self.data:byte(index+1,index+2)
+	if tm[h*256+l]~= nil then
+		if tm[h*256+l]==dir then return false end
+	end
+	--can we move onto a new tile?
+	index=math.floor(y2/tileSize)*self.size*2+math.floor(x2/tileSize)*2
 	l,h=self.data:byte(index+1,index+2)
 	return tf[h*256+l]	
 end
 
-function Node:draw()
+-- v(x,y) = view coordinates, b = background tile
+function Node:draw(vx,vy,b)
+	--find out the i and j indeces for the first visible tile
+	local i,j       =math.floor(math.max(vx-self.x,0)/tileSize),math.floor(math.max(vy-self.y,0)/tileSize)
+	--from i and j calculate the flat index of the first visible tile
+	local ix1=j*self.size*2+i*2+1
+	--calculate the position of first tile in screen coordinates
+	local x1,y=i*tileSize+(self.x-vx),j*tileSize+self.y-vy
 	--find out the visible boundaries
-	local i,j       =math.floor(math.max(world.x-self.x,0)/tileSize),math.floor(math.max(world.y-self.y,0)/tileSize)
-	local i2,j2,ii  =math.min(self.x2,i+9),math.min(self.y2,j+6),i
-	local dx,dy = self.x-world.x,self.y-world.y
-	local index,l,h
-	--5 rows
-	while j<j2 do
-		i=ii
-		index=j*self.size*2+i*2+1
-		--9 columns
-		while i<i2 do
-			l,h=self.data:byte(index,index+1)
-			td[h*256+l](i*tileSize+dx,j*tileSize+dy)
-			i=i+1;index=index+2
+	local xm,ym=math.min(self.size,i+9)*tileSize+(self.x-vx),math.min(self.size,j+6)*tileSize+(self.y-vy)
+	local l,h,x,ix
+	while y<ym do
+		x=x1
+		ix=ix1
+		while x<xm do
+			l,h=self.data:byte(ix,ix+1)
+			b:safeDraw(x,y,0,0,48,48)
+			td[h*256+l](x,y)
+			x=x+tileSize;ix=ix+2		
 		end
-		j=j+1
+		y=y+tileSize
+		ix1=ix1+self.size*2 --next row index
 	end
 end
 
 node=Node.new()
+
+world[0]=node --our world is just 1 node
 
 node:fill()
 
@@ -151,18 +251,20 @@ node:set(4,2,1)
 node:set(4,2,2)
 
 
-for i=2,5 do
+for i=0,31 do
 	node:set(1,i,0)
+	--node:set(1,i,31)
+	--node:set(1,31,i)
 end
 node:set(5,2,0)
 node:set(7,6,0)
 node:set(8,6,1)
 node:set(8,6,2)
 node:set(9,6,3)
-
-for i=0,31 do
-node:set(10,6,i)
-node:set(11,7,i)
+node:set(12,3,7)
+for i=0,29 do
+node:set(10,7,i)
+node:set(11,8,i)
 
 end
 
@@ -176,20 +278,27 @@ roadImg=image.load("gfx/apple.png");
 grassImg=image.load("grasspack.png");
 treeImg=image.load("gfx/water.png");
 grassImg=image.load("gfx/tileset.png");
+houseImg=image.load("gfx/houses/house1.png");
+trImg=image.load("gfx/tree.png");
 
 --tiles tables
 td={ [0]=function(x,y)  end,[1]=function(x,y) fenceImg:safeDraw(x,y,0,0,48,48) end,[2]=function(x,y) fenceImg:safeDraw(x,y,0,48,48,48)  end ,
 [3]=function(x,y) fenceImg:safeDraw(x,y,0,96,48,48)  end,[4]=function(x,y) fenceImg:safeDraw(x,y,48,48,48,48) end,[5]=function(x,y) fenceImg:safeDraw(x,y,48,0,48,48) end,
 [6]=function(x,y) fenceImg:safeDraw(x,y,48,96,48,48) end,[7]=function(x,y) fenceImg:safeDraw(x,y,96,0,48,48) end,[8]=function(x,y) fenceImg:safeDraw(x,y,96,48,48,48) end,
-[9]=function(x,y) fenceImg:safeDraw(x,y,96,96,48,48) end,[10]=function(x,y) treeImg:safeDraw(x,y,0,48,48,48) end,[11]=function(x,y) treeImg:safeDraw(x,y,96,48,48,48) end
+[9]=function(x,y) fenceImg:safeDraw(x,y,96,96,48,48) end,[10]=function(x,y) treeImg:safeDraw(x,y,0,48,48,48) end,[11]=function(x,y) treeImg:safeDraw(x,y,96,48,48,48) end,
+[12]=function(x,y) treeImg:safeDraw(x,y,144,0,48,48);treeImg:safeDraw(x+48,y,48,96,48,48);treeImg:safeDraw(x,y+48,96,48,48,48);treeImg:safeDraw(x,y+96,144,48,48,48);treeImg:safeDraw(x+48,y+96,48,0,48,48);treeImg:safeDraw(x+96,y+96,144,144,48,48);treeImg:safeDraw(x+96,y+48,0,48,48,48);treeImg:safeDraw(x+144,y+48,96,48,48,48);treeImg:safeDraw(x+96,y,144,96,48,48); end
 }
-tf={ [0]=true,[1]=false,[2]=true ,[4]=true}
+tf={ [0]=true,[4]=true,[8]=true} --available to stand?
+tm={ [4]=1,[8]=2 }
 
+td[13]=function(x,y) houseImg:safeDraw(x,y,0,0,48,48);houseImg:safeDraw(x+48,y,0,48,48,48) end
+node:set(13,3,6)
+td[14]=function(x,y) trImg:safeDraw(x,y,0,0,48,48); end
+td[15]=function(x,y) trImg:safeDraw(x,y,0,48,48,48); end
+node:set(14,5,6)
+node:set(15,5,7)
 
 function Redraw()
-	
-	world.background:safeDraw(0,0,tileSize- world.x % tileSize,tileSize- world.y % tileSize,ScreenWidth,ScreenHeight)
-	
 	--just debug
 	function grid()
 		local lc=color.new(255,0,0)
@@ -203,20 +312,17 @@ function Redraw()
 	end 
 	--grid()
 
-	node:draw()
+	world:draw()
+	
 		
 	drawCharacter(enemy)
 	drawCharacter(hero)
 	
-	function bar(x,y,w,h,v,c)
-		screen.fillrect(x,y,w,h,dialogBackColor)
-		screen.fillrect(x,y,v*w,h,c)
-		screen.drawrect(x,y,x+w-1,y+h-1,dialogOutColor)
-	end
+
 	
-	bar(4,4,90,10,hero.hp/100,healthbarColor)
+	drawBar(4,4,90,10,hero.hp/100,healthbarColor)
 	
-	--print(renderTime,gcinfo())
+	--dbg
 	text.size(15)
 	text.color(dialogTextColor)
 	text.draw(0,16,renderTime..','..globalTime)
@@ -225,7 +331,6 @@ function Redraw()
 	if dialogVisible then dialogDraw() end
 
 	screen.update()
-	
 end
 	
 function osms() 
@@ -255,11 +360,7 @@ function update()
 	
 	--update timers
 	globalTime=globalTime+dt
-	if dialogVisible==true then
-		dialogTime=dialogTime+dt
-		--print(dialogTime)
-		if dialogTime>=dialogDuration then dialogVisible=false;redraw() end
-	end
+	timer.update(dt)
 end
 
 oldOrientation=screen.orientation()
@@ -273,8 +374,8 @@ while true do
 		if control.isTouch()==1 then
 			CurX,CurY=touch.pos()
 			if touch.down()==1 then
-				SwipeX=CurX-(hero.x-world.x)
-				SwipeY=CurY-(hero.y-world.y)
+				SwipeX=CurX-(hero.x-viewx)
+				SwipeY=CurY-(hero.y-viewy)
 				if math.abs(SwipeY)>math.abs(SwipeX) then
 					if SwipeY<0 then hero.dir=3;SwipeY=-tileSize else hero.dir=0;SwipeY=tileSize end
 					SwipeX=0
